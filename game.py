aa161a2e9604a5204cfdb1eea838cb583a7cf6d9
Lana Tinawi <ltinawi@luc.edu>
2023-04-01 16:14:37 -0500

    Lanas first commit

--- game.py
+++ game.py
@@ -11,7 +11,7 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bacteria Game!")
 
-#main game loop
+# main game loop
 running = True
 while running:
 
@@ -20,3 +20,11 @@ while running:
             running = False
 
     pygame.display.update()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 30))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()  # checks images and get rect... self.rect.center = (winWidth / 2, winHeight / 2) #self.rect.bottom = winHeight

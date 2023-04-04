c32694d474b8c744180ff2d4b759b072cf13aa9d
charlesbueso <37254269+charlesbueso@users.noreply.github.com>
2023-04-04 15:08:43 -0500

    gamebox, player fixated to mouse

--- game.py
+++ game.py
@@ -3,30 +3,40 @@ import time
 import math
 import random
 
-WIDTH = 900
-HEIGHT = 750
-
 pygame.init()
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+WIDTH = 900
+HEIGHT = 750
+fps = 60
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
 pygame.display.set_caption("Bacteria Game!")
+timer = pygame.time.Clock()
+
 
 # main game loop
 running = True
 while running:
 
+    screen.fill('white')
+    timer.tick(fps)
+    mousePosition = pygame.mouse.get_pos()
+    pygame.draw.circle(screen, 'red', mousePosition, 10)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    pygame.display.update()
+    pygame.display.flip()
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((30, 30))
-        self.image.fill(WHITE)
+        self.image.fill('white')
         self.rect = self.image.get_rect()
         # checks images and get rect... self.rect.center = (winWidth / 2, winHeight / 2) #self.rect.bottom = winHeight
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
+
+
+pygame.quit()

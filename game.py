68b24c0a0afeebb4782ca83829ebf5cbb63b00a4
charlesbueso <37254269+charlesbueso@users.noreply.github.com>
2023-04-07 14:27:33 -0500

    menu screen, transition to gameplay

--- game.py
+++ game.py
@@ -30,20 +30,21 @@ class Bacteria(pygame.sprite.Sprite):
 
         # current mouse position
         mouse_x, mouse_y = pygame.mouse.get_pos()
-            
+
         dx = mouse_x - self.rect.centerx
         dy = mouse_y - self.rect.centery
         dist = ((dx ** 2) + (dy ** 2)) ** 0.5 # distance between the mouse and the bacteria (Pythagorean theorem)
-        
+
         if dist > self.speed:
             self.rect.centerx += dx * self.speed / dist
             self.rect.centery += dy * self.speed / dist
         else:
             self.rect.centerx = mouse_x
-            self.rect.centery = mouse_y
+            self.rect.centery = mouse_y   
 
+    #Missing update function
+    #Will update movement of antidotes, as well as random spawns(?) and size mutations(?)
         
-
 class Antidote(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -59,7 +60,8 @@ class Antidote(pygame.sprite.Sprite):
     #Missing update function
     #Will update movement of antidotes, as well as random spawns(?) and size mutations(?)
     #def update():
-         
+
+
 class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
@@ -92,38 +94,87 @@ class CameraGroup(pygame.sprite.Group):
             self.display_surface.blit(sprite.image,offset_pos)
 
 
+def game_loop():
+    #intro = False
+    running = True
+    while running:
+
+        screen.fill('white')
+        timer.tick(fps)
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+    
+        camera.update()
+        camera.custom_draw(bacteria)
+
+        all_sprites_list.update()
+        all_sprites_list.draw(screen)
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.update()
+        pygame.display.flip()
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, 'black')
+    return textSurface, textSurface.get_rect()
+
+def button(message,x,y,width,height,inactiveColor,activeColor,action=None):
+    global gameRunning
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+width > mouse_x > x and y+height > mouse_y > y:
+        pygame.draw.rect(screen, activeColor,(x,y,width,height))
+
+        if click[0] == 1 and action != None:
+            action()
+    else:
+        pygame.draw.rect(screen, inactiveColor,(x,y,width,height))
+
+    smallText = pygame.font.Font("playbuttonfont.ttf",40)
+    textSurf, textRect = text_objects(message, smallText)
+    textRect.center = ((x+(width/2)), (y+(height/2)))
+    screen.blit(textSurf, textRect)
+
+def game_intro():
+
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        screen.fill('white')
+        largeText = pygame.font.Font('titlefont.ttf',100)
+        TextSurf, TextRect = text_objects("Bacteria Game", largeText)
+        TextRect.center = ((WIDTH/2),(HEIGHT/2))
+        screen.blit(TextSurf, TextRect)
+
+        button("PLAY",385,515,140,75,"yellow","green",game_loop)
+
+        pygame.display.update()
+        timer.tick(15)
+
 
 pygame.init()
 
 all_sprites_list = pygame.sprite.Group()
+
 bacteria = Bacteria()
+all_sprites_list.add(bacteria)
 Bacteria.image = pygame.Surface([30, 30])
 Bacteria.rect = Bacteria.image.get_rect()
-camera = CameraGroup()
-all_sprites_list.add(bacteria)
 
+camera = CameraGroup()
 mutationCounter = 0
 
-
-# main game loop
-running = True
-while running:
-
-    screen.fill('white')
-    timer.tick(fps)
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    
-    camera.update()
-    camera.custom_draw(bacteria)
-
-    all_sprites_list.update()
-    all_sprites_list.draw(screen)
-    
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    pygame.display.flip()
-
+# menu
+game_intro()
+# main loop
+game_loop()
+# quit
 pygame.quit()
